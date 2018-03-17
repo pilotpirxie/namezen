bf5408d8220bbc0aaff5bea37a06697e5f819615
pilotpirxie <10637666+pilotpirxie@users.noreply.github.com>
2018-03-18 00:03:54 +0100

    Filled tests

--- test/test.js
+++ test/test.js
@@ -1,9 +1,11 @@
 // http://www.chaijs.com/guide/styles
 const assert = require('chai').assert;
+const Namezen = require('../lib/namezen');
+const nmz = new Namezen();
 
 describe('Name Generator', function() {
   describe('Generating 5 random names', function() {
-    var names = [];
+    var names = nmz.generate('chips,potato,crispy,fast,food,burger,meal', 5, 12, 5);
 
     it('should list 5 random names', function() {
       assert.lengthOf(names, 5);
